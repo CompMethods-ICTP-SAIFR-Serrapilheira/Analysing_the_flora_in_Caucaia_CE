a85ef26b151bc6cacca9097b623cedb7cd92a85c
Cristiananc <cristiananc@hotmail.com>
2022-08-18 01:52:20 -0300

    adding script file for the next step in the analysis

---
+++ R/02_calculating_richness.R
@@ -0,0 +1,8 @@
+#Date: August 17, 2022
+#Author: Cristiana Aparecida Nogueira Couto
+#This script calculates the richness of the municipality of Caucaia
+#Both considering the whole city, as well as dividing by the different
+#vegetation, according to the registered data collected from the herbariums. 
+
+
+
