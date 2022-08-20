167b245c57f978e38512adece850de40d8f246df
Cristiananc <cristiananc@hotmail.com>
2022-08-20 17:07:00 -0300

    starting script to plot richness

---
+++ R/04_plotting_richness.R
@@ -0,0 +1,12 @@
+#Date: August 20, 2022
+#Author: Cristiana Aparecida Nogueira Couto
+#This script plots the richness by genre in Caucaia
+
+library(ggplot2)
+
+#Importing the data
+flora_genre <- read.csv("data/output/03_flora_by_genre.csv", header=TRUE)
+
+#Plotting the data ----------------
+
+
