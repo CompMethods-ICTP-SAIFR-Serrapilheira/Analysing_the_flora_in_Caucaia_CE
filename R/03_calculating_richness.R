c689faeb221b803d99f39b5d9bf1e8d1160f3b33
Cristiananc <cristiananc@hotmail.com>
2022-08-21 10:59:26 -0300

    adding the files for report

--- R/03_calculating_richness.R
+++ R/03_calculating_richness.R
@@ -16,49 +16,50 @@ sum(flora_table$municipio == "Caucaia", na.rm = TRUE)
 #species found in Caucaia, so we add the name for all of them
 flora_table$municipio[which(is.na(flora_table$municipio))] <- "Caucaia"
 
-#Richness by genre -------------
+#Richness by genus -------------
 sort(unique(flora_table$genero))
+flora_table$genero <-  trimws(flora_table$genero)
 
-#Now we check the genre rows with the taxonomists names, we want to combine them with the rows 
-#with only the genre name
-taxonomists_genre <- "L.$|Vahl$|Aubl.$|Jacq.$|R.Br$|ex A.Juss.$|Mart. ex Klotzsch$"
-unique(flora_table$genero[which(stri_detect_regex(flora_table$genero, taxonomists_genre))])
+#Now we check the genus rows with the taxonomists names, we want to combine them with the rows 
+#with only the genus name
+taxonomists_genus <- "L.$|Vahl$|Aubl.$|Jacq.$|R.Br$|ex A.Juss.$|Mart. ex Klotzsch$"
+unique(flora_table$genero[which(stri_detect_regex(flora_table$genero, taxonomists_genus))])
 
-genre_regex <- c("Cyperus", "Actinostemon", "Ditaxis", "Lycium", "Trigonia", "Tamonea", 
+genus_regex <- c("Cyperus", "Actinostemon", "Ditaxis", "Lycium", "Trigonia", "Tamonea", 
                  "Fimbristylis", "Croton", "Jatropha", "Schwenckia", "Lantana", "Hybanthus", 
                  "Dioscorea", "Dalechampia", "Chrysophyllum", "Jacquinia", "Stachytarpheta",
                  "Eleocharis", "Manihot")
 
-flora_genre_aux <- flora_table %>%
+flora_genus_aux <- flora_table %>%
   mutate(genero = 
            case_when(
-              stri_detect_regex(genero, genre_regex[1]) ~ genre_regex[1],
-              stri_detect_regex(genero, genre_regex[2]) ~ genre_regex[2],
-              stri_detect_regex(genero, genre_regex[3]) ~ genre_regex[3],
-              stri_detect_regex(genero, genre_regex[4]) ~ genre_regex[4],
-              stri_detect_regex(genero, genre_regex[5]) ~ genre_regex[5],
-              stri_detect_regex(genero, genre_regex[6]) ~ genre_regex[6],
-              stri_detect_regex(genero, genre_regex[7]) ~ genre_regex[7],
-              stri_detect_regex(genero, genre_regex[8]) ~ genre_regex[8],
-              stri_detect_regex(genero, genre_regex[9]) ~ genre_regex[9],
-              stri_detect_regex(genero, genre_regex[10]) ~ genre_regex[10],
-              stri_detect_regex(genero, genre_regex[11]) ~ genre_regex[11],
-              stri_detect_regex(genero, genre_regex[12]) ~ genre_regex[12],
-              stri_detect_regex(genero, genre_regex[13]) ~ genre_regex[13],
-              stri_detect_regex(genero, genre_regex[14]) ~ genre_regex[14],
-              stri_detect_regex(genero, genre_regex[15]) ~ genre_regex[15],
-              stri_detect_regex(genero, genre_regex[16]) ~ genre_regex[16],
-              stri_detect_regex(genero, genre_regex[17]) ~ genre_regex[17],
-              stri_detect_regex(genero, genre_regex[18]) ~ genre_regex[18],
-              stri_detect_regex(genero, genre_regex[19]) ~ genre_regex[19],
+              stri_detect_regex(genero, genus_regex[1]) ~ genus_regex[1],
+              stri_detect_regex(genero, genus_regex[2]) ~ genus_regex[2],
+              stri_detect_regex(genero, genus_regex[3]) ~ genus_regex[3],
+              stri_detect_regex(genero, genus_regex[4]) ~ genus_regex[4],
+              stri_detect_regex(genero, genus_regex[5]) ~ genus_regex[5],
+              stri_detect_regex(genero, genus_regex[6]) ~ genus_regex[6],
+              stri_detect_regex(genero, genus_regex[7]) ~ genus_regex[7],
+              stri_detect_regex(genero, genus_regex[8]) ~ genus_regex[8],
+              stri_detect_regex(genero, genus_regex[9]) ~ genus_regex[9],
+              stri_detect_regex(genero, genus_regex[10]) ~ genus_regex[10],
+              stri_detect_regex(genero, genus_regex[11]) ~ genus_regex[11],
+              stri_detect_regex(genero, genus_regex[12]) ~ genus_regex[12],
+              stri_detect_regex(genero, genus_regex[13]) ~ genus_regex[13],
+              stri_detect_regex(genero, genus_regex[14]) ~ genus_regex[14],
+              stri_detect_regex(genero, genus_regex[15]) ~ genus_regex[15],
+              stri_detect_regex(genero, genus_regex[16]) ~ genus_regex[16],
+              stri_detect_regex(genero, genus_regex[17]) ~ genus_regex[17],
+              stri_detect_regex(genero, genus_regex[18]) ~ genus_regex[18],
+              stri_detect_regex(genero, genus_regex[19]) ~ genus_regex[19],
               
               TRUE ~ genero
              ))
 
-#Now, we calculate the richness by genre
-flora_by_genre <- flora_genre_aux %>% 
+#Now, we calculate the richness by genus
+flora_by_genus <- flora_genus_aux %>% 
   group_by(genero) %>%
-  summarise(richness_by_genre = n())
+  summarise(richness_by_genus = n())
   
-#Exporting the flora table by genre
-write.table(flora_by_genre, file = "data/output/03_flora_by_genre.csv", sep = ",")
+#Exporting the flora table by genus
+write.table(flora_by_genus, file = "data/output/03_flora_by_genus.csv", sep = ",")
--- R/04_plotting_richness.R
+++ R/04_plotting_richness.R
@@ -1,12 +1,35 @@
 #Date: August 20, 2022
 #Author: Cristiana Aparecida Nogueira Couto
-#This script plots the richness by genre in Caucaia
+#This script plots the richness by genus in Caucaia
 
 library(ggplot2)
+library(tidyverse)
 
 #Importing the data
-flora_genre <- read.csv("data/output/03_flora_by_genre.csv", header=TRUE)
+flora_genus <- read.csv("data/output/03_flora_by_genus.csv", header=TRUE)
 
 #Plotting the data ----------------
+flora_genus_aux <- flora_genus %>%
+  arrange(desc(richness_by_genus)) %>% 
+  filter_all(any_vars(!is.na(genero)))
 
+flora_genus_first_30 <- flora_genus_aux[1:30,]
 
+#The number of genus found was 312
+length(flora_genus_aux$genero)
+
+#Plotting the data
+p <- ggplot(data = flora_genus_first_30, aes(x = reorder(genero, -richness_by_genus), y = richness_by_genus)) + 
+  geom_bar(stat = "identity", fill="#a2de70") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Genus", y = "Number of occurences") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#Richness by genus in Caucaia-CE
+p
+
+#Exporting figure
+ggsave(path = "figs", 
+       filename = "04_richness_by_genus.png", 
+       plot = p, device = 'png')
